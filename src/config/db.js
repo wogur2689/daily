6b414ef6a3d07405a4cf1a3094e3e27af76af9dd
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2023-01-21 21:32:59 +0900

    [refator] node_module 제거

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql"); 
+
+const db = mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: '1234',
+        database: 'daily'
+})
+
+module.exports = db;
