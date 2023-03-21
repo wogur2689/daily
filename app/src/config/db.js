e2ea96cfdbe31e9177ea1b97518ba4d6a3d8b018
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2023-03-21 12:34:38 +0900

    [feat] nodemon, mysql2 추가

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: "localhost",
     port: '3306',
     user: "root",
-    password: "1234",
+    password: "1225",
     database: "daily",
 });
 
