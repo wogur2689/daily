892c240b34cece72263cb23b3560b2d2ba3e486a
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2023-03-24 02:15:46 +0900

    [fix] 문구 변경

--- app/bin/www.js
+++ app/bin/www.js
@@ -5,6 +5,7 @@ const app = require("../app");
 const logger = require("../src/config/logger"); //로그 관리 모듈
 
 const PORT = process.env.PORT || 3000; //서버 포트 default 3000
+/* 이프로젝트는 로컬 프로젝트라 env설정은 따로 없음. */
 
 app.listen(PORT, () => {
     logger.info(`${PORT} 포트에서 서버가 가동되었습니다.`);
